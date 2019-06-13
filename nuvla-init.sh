5e00d64819676b2be92707cd647bd55c1f6a3792
Charles (Cal) Loomis <cal@sixsq.com>
2019-06-13 16:40:35 +0200

    fix path

--- nuvla-init.sh
+++ nuvla-init.sh
@@ -2,11 +2,11 @@
 
 bin/elasticsearch-keystore create
 
-if [ -f "/run/secrets/s3_access_key" ]; then
+if [ -f "/var/run/secrets/s3_access_key" ]; then
   cat /run/secrets/s3_access_key | bin/elasticsearch-keystore add --stdin s3.client.default.access_key
 fi
 
-if [ -f "/run/secrets/s3_secret_key" ]; then
+if [ -f "/var/run/secrets/s3_secret_key" ]; then
   cat /run/secrets/s3_secret_key | bin/elasticsearch-keystore add --stdin s3.client.default.secret_key
 fi
 
